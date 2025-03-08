5fe7be9e8e4463e044eea6a7c10ffe25c29444cc
Max Vallim <max.vallim@gmail.com>
2025-03-08 11:38:58 -0300

    Atualizacao c++
    novo rumo

---
+++ atividades_C++/atv1/Atv1_1.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// Função que representa a atividade 1
+void atividade1() {
+    cout << "Esta é a atividade 1" << endl;
+
+    // Adicione aqui o código específico da sua atividade
+    int a, b;
+    cout << "Digite dois números: ";
+    cin >> a >> b;
+    cout << "A soma dos números é: " << (a + b) << endl;
+}
